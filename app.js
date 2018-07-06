5255389cf789e5c1d389e4fae886038d23af3648
faithngetich <faithngetich188@gmail.com>
2018-07-06 11:37:55 +0300

    Use the console

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('Hello world');
+console.log('Hello World!');
+console.log(123);
+console.log(true);
+console.log({a:123, b:3});
+console.table({a:123, b:3});
+console.error({a:123, b:3});
+console.warn({a:123, b:3});
+console.time('Hello');
+  console.table({a:123, b:3});
+  console.error({a:123, b:3});
+  console.warn({a:123, b:3});
+console.timeEnd('Hello');
