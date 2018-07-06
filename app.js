8ab663eff47f17233d918905fd138c42194bb98e
faithngetich <faithngetich188@gmail.com>
2018-07-06 17:43:22 +0300

    Arrays & Array Methods

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
-const name = 'William';
-const age = 45;
-const job = 'Web developer';
-const city = 'Miami';
-let html;
+const numbers = [43,56,33,23,44,36,5];
+const numbers2 = new Array(33,4,5,4);
+const fruit = ['Apple', 'po', 'Banana', 'Orange'];
+const mixed = [null, 5, 'ff', {a:2}, new Date()]
 
-// Without template strings (es5)
-html = '<ul>' +
-       ' <li>Name: ' + name + '</li>' +
-       ' <li>Age: ' + age +  '</li>' +
-       ' <li>Job: ' + job + ' </li>' +
-       ' <li>City: '+ city +' </ul>' +
-       ' </ul>';
+let val;
 
-function hello(){
-  return 'hello';
-}
-// With template strings (es6)
-html = `
-  <ul>
-    <li>Name: ${name}</li>
-    <li>:Age ${age}</li>
-    <li>:Job ${job}</li>
-    <li>:City ${city}</li>
-    <li>${2 + 2}</li>
-    <li>${hello()}</li>
-    <li>${age > 30 ? 'Over 30' : 'Under 30' }</li>
-  </ul>
-    `
-;
+// GEt array length
+val = numbers.length;
+// Check if if array
+val = Array.isArray(numbers);
+// Get single value
+val = numbers[3];
+// insert into arrays
+numbers[2] = 100;
+// Find index of value
+val = numbers.indexOf(36);
 
-document.body.innerHTML = html;
+// MUTATING ARRAYA
+//Add on to end
+numbers.push(3);
+// Add on to front
+numbers.unshift(120);
+// Take off from end
+numbers.pop();
+// Take off from the front
+numbers.shift();
+// Splice values
+numbers.splice(1,3);
+// Reverse
+numbers.reverse();
+
+// Concatenate array
+val = numbers.concat(numbers2);
+
+// Sorting arrays
+val = fruit.sort();
+val = numbers.sort();
+
+// Use the compare function
+val = numbers.sort(function(x, y){
+  return x - y;
+});
+
+// Reverse sort
+val = numbers.sort(function(x, y){
+  return y - x;
+});
+
+// Find
+function under50(num){
+  return num < 50;
+};
+
+val = numbers.find(under50);
+
+console.log(numbers);
+console.log(val);
