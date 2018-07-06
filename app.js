5a4adac01f777e875967cef0bd258f47380fc784
faithngetich <faithngetich188@gmail.com>
2018-07-06 21:04:15 +0300

    switches

--- app.js
+++ app.js
@@ -1,83 +1,41 @@
-const id = 100;
+const color = 'red';
 
-// // EQUAL TO
-// if(id == 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT')
-// }
-
-// // Not EQUAL TO
-// if(id != 101){
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT')
-// }
-
-// // EQUAL TO VALUE AND TYPE
-// if(id === 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT')
-// }
-
-// // NOT EQUAL TO VALUE AND TYPE
-// if(id !== 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT')
-// }
-
-// TEst if undefined
-// if(typeof id !== 'undefined'){
-//   console.log(`The ID is ${id}`);
-// } else {
-//   console.log('No ID')
-// }
-
-// Greater or less than
-// if(id >= 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT')
-// }
-
-// IF ELSE
- 
-const color = 'Yellow';
-
-if(color === 'red'){
-  console.log('Color is red')
-}else if(color === 'blue'){
-  console.log('Color is blue');
-}else{
-  console.log('Color is not red or blue');
+switch(color){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('Color is not red or blue');
+    break;
 }
 
-// Logical operators
-const name = 'Steve';
-const age = 15;
+let day;
 
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`)
-} else if(age >= 13 && age <= 19){
-  console.log`${name} is a teenager`
-}else{
-  console.log(`${name} is an adult`)
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break
+  case 1:
+    day = 'Monday';
+    break
+  case 2:
+    day = 'Tuesday';
+    break
+  case 3:
+    day = 'Wednesday';
+    break
+  case 4:
+    day = 'Thursday';
+    break
+  case 5:
+    day = 'Friday';
+    break
+  case 6:
+    day = 'Saturday';
+    break
 }
 
-// OR ||
-if(age < 16 || age < 65){
-  console.log(`${name} cannot run in race`)
-}else{
-  console.log(`${name} is registered for the race`)
-}
-
-// TERNARY operators
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT')
-
-// Without braces
-if(id === 100)
-  console.log('CORRECT');
-else
-  console.log('INCORRECT');
+console.log(`Today is ${day}`);
