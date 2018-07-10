caf0f29d7d47a8f12b773160bc23cc2ea4ba3aae
faithngetich <faithngetich188@gmail.com>
2018-07-10 17:51:59 +0300

    extending classes

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
-// // set local storage item
-// localStorage.setItem('name', 'John');
-// localStorage.setItem('age', '30');
+class Person {
+  constructor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
 
-// // set session storage item
-// sessionStorage.setItem('name', 'Beth');
-
-// remove from storage
-// localStorage.removeItem('name')
-
-// // get from storage
-// const name = localStorage.getItem('name');
-// const age = localStorage.getItem('age');
-
-// // clear local storage
-// localStorage.clear();
-
-// console.log(name, age);
-
-document.querySelector('form').addEventListener('submit',
-function(e){
-  const task = document.getElementById('task').value;
-
-  let tasks;
-
-  if(localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
 
-  tasks.push(task);
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+}
 
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
 
-  alert('Task saved')
+    this.phone = phone;
+    this.membership = membership;
+  }
 
-  e.preventDefault();
-})
+  static getMembershipCost(){
+    return 500;
+  }
+}
 
-const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-tasks.forEach(function(task){
-  console.log(task);
-})
+const john = new Customer('John', 'Doe', '555-555-5555', 'Satandard');
+
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
